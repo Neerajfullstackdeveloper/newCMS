4d860ab0cdaa0b10dcecc97a1b2e813ab8cdb4c0
Neeraj Pandey <nppandey2003@gmail.com>
2025-09-05 07:18:03 +0530

    hide sign up

--- client/src/pages/auth-page.tsx
+++ client/src/pages/auth-page.tsx
@@ -83,7 +83,7 @@ export default function AuthPage() {
           <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
             <TabsList className="grid w-full grid-cols-2 mb-6">
               <TabsTrigger value="login">Sign In</TabsTrigger>
-              <TabsTrigger value="register">Sign Up</TabsTrigger>
+              {/* <TabsTrigger value="register">Sign Up</TabsTrigger> */}
             </TabsList>
 
             <TabsContent value="login">
